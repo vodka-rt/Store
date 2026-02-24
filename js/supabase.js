6a49fa3d96326beb2b8cdd569120aafd9025fa2f
Vodka <diogojoaquimdemelosantana@gmail.com>
2026-02-24 17:59:41 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,5 @@
+// js/supabase.js
+const SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
+const SUPABASE_ANON_KEY = "SUA_ANON_KEY";
 
+export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
